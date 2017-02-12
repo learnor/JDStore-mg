3145d054fe7979efb630adc343c27f82fee6ab83
Learnor <learnor@gmail.com>
2017-02-12 05:33:51 -0600

    only admin can access backend panel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+u = User.create(email: "admin@test.com", password: "000000", password_confirmation: "000000", is_admin: true)
+puts "admin user #{u.email} created, password is #{u.password}"
