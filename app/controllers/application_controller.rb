d99e7f53e127cd2a5d1ad67abf0e981633b09b46
Zion <learnor@gmail.com>
2017-02-13 23:55:40 -0600

    Revert "Devise enhance"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def admin_required
     if !current_user.admin?
@@ -24,10 +23,4 @@ class ApplicationController < ActionController::Base
     session[:cart_id] = cart.id
     return cart
   end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
-  end
-
 end
--- db/migrate/20170213044921_add_name_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToUser < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :nickname, :string, null: false, default: ""
-  end
-end
