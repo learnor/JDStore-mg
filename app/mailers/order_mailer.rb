83bedac9f9f92b1786cf5745cb4d3311e039caeb
Learnor <learnor@gmail.com>
2017-02-14 16:33:16 -0600

    add apply_cancel function

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -6,4 +6,12 @@ class OrderMailer < ApplicationMailer
 
     mail(to: @user.email, subject: "【海盗学院】感谢您完成本次的下单，以下是您这次购物明细 #{order.token}")
   end
+
+  def apply_cancel(order)
+    @order = order
+    @user = @order.user
+    @product_lists = order.product_lists
+# TODO: change admin@test.com email
+    mail(to: "admin@test.com", subject: "【海盗学院】用户#{order.user.email}申请取消订单 #{order.token} ")
+  end
 end
