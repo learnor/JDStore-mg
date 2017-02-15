d18d4ae0e202c9dd52a75be24ca90b87ec1ccfad
Learnor <learnor@gmail.com>
2017-02-15 12:57:15 -0600

    user need to confirm email to sign up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215115536) do
+ActiveRecord::Schema.define(version: 20170215123516) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -75,6 +75,10 @@ ActiveRecord::Schema.define(version: 20170215115536) do
     t.datetime "updated_at",                             null: false
     t.boolean  "is_admin",               default: false
     t.string   "username"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
