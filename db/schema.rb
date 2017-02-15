117ad388a5ecb456de766f746b275f7ec8bb66a1
Learnor <learnor@gmail.com>
2017-02-15 06:27:12 -0600

    Add username/can use username login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214200638) do
+ActiveRecord::Schema.define(version: 20170215115536) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -74,8 +74,10 @@ ActiveRecord::Schema.define(version: 20170214200638) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "is_admin",               default: false
+    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
