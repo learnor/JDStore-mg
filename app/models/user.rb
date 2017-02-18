4a5844e8dce553605927c1c0e269e8f16bbf19fc
Learnor <learnor@gmail.com>
2017-02-18 16:02:45 -0600

    enable email confirmation, change confirm mail style

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }
   attr_accessor :login
