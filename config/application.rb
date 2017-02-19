b76c5943a0843defa1e20c2edaa6297a3d72fe3f
Learnor <learnor@gmail.com>
2017-02-18 19:37:08 -0600

    add I18n for devise

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,6 @@ module JDStoreMg
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :"zh-CN"
   end
 end
