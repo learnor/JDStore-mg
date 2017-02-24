23ecbb5500dfbf29292c395c1add7c61340f0d7c
Learnor <learnor@gmail.com>
2017-02-24 13:41:20 -0600

    add validation to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,7 @@ class Product < ApplicationRecord
   has_many :favorites
   has_many :fans, through: :favorites, source: :user
   belongs_to :category
+
+  validates :title, :description, :quantity, :category, :price, presence: true
+  validates :quantity, :price, numericality: { greater_than_or_equal_to: 0 }
 end
