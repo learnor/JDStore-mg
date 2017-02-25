c4b15f5aa1b7dfba3ae51af3256bda9f7587d120
maggie <maggie20161225@gmail.com>
2017-02-25 23:30:02 +0800

    navbar change

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,4 +6,7 @@ class WelcomeController < ApplicationController
   def home
 
   end
+  def profile
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,42 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :categories
-    resources :products
+    namespace :admin do
+        resources :categories
+        resources :products
+        resources :orders do
+            member do
+                post :cancel
+                post :ship
+                post :deliver
+                post :return
+            end
+        end
+    end
+    devise_for :users, controllers: { confirmations: 'confirmations' }
+    root 'welcome#home'
+    get 'profile' => 'welcome#profile'
+    resources :products do
+        member do
+            post :add_to_cart
+        end
+    end
+
+    resources :carts do
+        collection do
+            delete :clean
+            post :checkout
+        end
+    end
+
+    resources :cart_items
     resources :orders do
-      member do
-        post :cancel
-        post :ship
-        post :deliver
-        post :return
-      end
+        member do
+            post :pay_with_alipay
+            post :pay_with_wechat
+            post :apply_to_cancel
+        end
     end
-  end
-  devise_for :users, controllers: { confirmations: 'confirmations'}
-  root "welcome#home"
-  resources :products do
-    member do
-      post :add_to_cart
+    namespace :account do
+        resources :orders, :favorites, only: [:index]
     end
-  end
-  resources :carts do
-    collection do
-      delete :clean
-      post  :checkout
-    end
-  end
-  resources :cart_items
-  resources :orders do
-    member do
-      post :pay_with_alipay
-      post :pay_with_wechat
-      post :apply_to_cancel
-    end
-  end
-  namespace :account do
-    resources :orders, :favorites, only: [:index]
-  end
-  resources :favorites, only: [:create, :destroy]
+    resources :favorites, only: [:create, :destroy]
 end
