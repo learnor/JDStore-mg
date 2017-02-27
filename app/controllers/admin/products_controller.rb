f8374889dd62e3cea678d455c679e399ba24a493
maggie <maggie20161225@gmail.com>
2017-02-27 14:54:48 +0800

    fix cart delete

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -39,6 +39,10 @@ class Admin::ProductsController < ApplicationController
   def destroy
       @product = Product.find(params[:id])
       @product.destroy
+      @cart = current_cart
+      @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+      @product = @cart_item.product
+      @cart_item.destroy
     redirect_to admin_products_path, alert: 'Product deleted successfully.'
   end
   private
