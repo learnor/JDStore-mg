3dd3f0b839251303ceef9564e855565b4ffc4cf1
Learnor <learnor@gmail.com>
2017-03-02 03:36:33 -0600

    add will_pagination/favorite function

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,23 +1,24 @@
 class FavoritesController < ApplicationController
-    before_action :set_product
+  before_action :authenticate_user!
+  before_action :set_product
 
-    def create
-        if Favorite.create(product: @product, user: current_user)
-            redirect_to :back, notice: "Product #{@product.title} has been favorited"
-        else
-            redirect_to :back, notice: "Something went wrong...favorite failed"
-        end
-    end
+  def create
+      if Favorite.create(product: @product, user: current_user)
+          redirect_to :back, notice: "Product #{@product.title} has been favorited"
+      else
+          redirect_to :back, notice: "Something went wrong...favorite failed"
+      end
+  end
 
-    def destroy
-        @favorite = Favorite.find_by(user: current_user,product: @product)
-        @favorite.destroy
-        redirect_to :back, alert: "Removed product #{@product.title} from favorite"
-    end
+  def destroy
+      @favorite = Favorite.find_by(user: current_user,product: @product)
+      @favorite.destroy
+      redirect_to :back, alert: "Removed product #{@product.title} from favorite"
+  end
 
-    private
+  private
 
-    def set_product
-        @product = Product.find(params[:product_id] || params[:id])
-    end
+  def set_product
+      @product = Product.find(params[:product_id] || params[:id])
+  end
 end
